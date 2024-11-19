5d72ddda4cf0cbdfa4faa9dad04e90ba1f95740c
KoDongUk <bogo1357@hanmail.net>
2024-11-19 17:05:37 +0900

    feature: quadrangle-weather-card 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { WideWeatherCard } from "@/components/wide-weather-card";
+import { QuadRangleWeatherCard } from "@/components/quadrangle-weather-card";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col bg-blue-500">
       <div className="w-full h-1/6 rounded-2xl border-2 border-white bg-white">
         <WideWeatherCard />
+        <QuadRangleWeatherCard />
       </div>
     </div>
   );
