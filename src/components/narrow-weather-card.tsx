6005a30f43c7b804e1281e81bacfe8b472f6c34d
KoDongUk <bogo1357@hanmail.net>
2024-11-27 18:50:47 +0900

    feature: 날씨, 시간 별 이미지 수정

--- src/components/narrow-weather-card.tsx
+++ src/components/narrow-weather-card.tsx
@@ -1,29 +1,45 @@
 'use client'
 
 import { useWeatherStore } from "@/stores/weather-store";
+import { WeatherSpriteIcon } from "./weather-sprite-icon";
 
 export const NarrowWeatherCard = () => {
   const {departureWeather, destinationWeather} = useWeatherStore();
 
+  const getWeather = (PTY: string) => {
+    switch(PTY) {
+      case '0':
+        return 'sunny';
+      case '2':
+        return 'rainySnow';
+      case '3':
+        return 'snow';
+      default:
+        return 'rainy'
+    }
+  }
+
   return (
-    <div className="grid grid-cols-3 pl-4 pr-4 gap-x-0 gap-y-4">
+    <div className="grid grid-cols-3 pl-4 pr-4 gap-x-0 gap-y-4 text-white">
       <div>
         {departureWeather.map((item, index) => (
-          <div key={index} className="flex items-center justify-center h-[5vh] border rounded-l-lg shadow-lg bg-white mb-4">
+          <div key={index} className="flex items-center justify-center h-[5vh] rounded-l-lg shadow-lg mb-4">
             {index+6}시
           </div>
         ))}
       </div>
       <div>
         {departureWeather.map((item, index) => (
-          <div key={index} className="flex items-center justify-center h-[5vh] border shadow-lg bg-white mb-4">
+          <div key={index} className="flex items-center justify-center h-[5vh] shadow-lg mb-4">
+            <WeatherSpriteIcon imgName={`${getWeather(item.PTY)}${index <= 13 ? 'Day' : 'Night'}`} />
             {item.PTY} {item.POP}
           </div>
         ))}
       </div>
       <div>
       {destinationWeather.map((item, index) => (
-          <div key={index} className="flex items-center justify-center h-[5vh] border rounded-r-lg shadow-lg bg-white mb-4">
+          <div key={index} className="flex items-center justify-center h-[5vh] rounded-r-lg shadow-lg mb-4">
+            <WeatherSpriteIcon imgName={`${getWeather(item.PTY)}${index <= 13 ? 'Day' : 'Night'}`} />
             {item.PTY} {item.POP}
           </div>
         ))}
