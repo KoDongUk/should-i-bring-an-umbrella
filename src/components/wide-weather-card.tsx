447c93e964a8527d320f5479ff72de1c7f423746
KoDongUk <bogo1357@hanmail.net>
2024-11-29 17:46:34 +0900

    style: 상단 요약 컴포넌트 스타일 설정

--- src/components/wide-weather-card.tsx
+++ src/components/wide-weather-card.tsx
@@ -15,7 +15,7 @@ export const WideWeatherCard = () => {
 
   return (
     <div className="gap-4 px-4 pt-4">
-      <div className="flex h-[10vh] items-center justify-center border rounded-lg p-4 shadow-lg bg-white">
+      <div className="flex h-[10vh] items-center justify-center border-2 rounded-lg p-4 shadow-lg text-white">
         {isRainyDay ?
           <div><p>오늘은 우산을 챙기셔야 할 것 같아요.</p></div> :
           <div><p>오늘은 우산을 챙길 필요가 없으실 것 같아요.</p></div>
